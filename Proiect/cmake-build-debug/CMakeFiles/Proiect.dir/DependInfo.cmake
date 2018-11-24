f58a64aa008b8d629972125ef87fadde80a54953
Calinciuc Andrei <scoala.andrei@yahoo.com>
2018-11-25 01:59:20 +0200

    std

--- Proiect/cmake-build-debug/CMakeFiles/Proiect.dir/DependInfo.cmake
+++ Proiect/cmake-build-debug/CMakeFiles/Proiect.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/andrei/CLionProjects/Retele/BigProject/Proiect/main.cpp" "/home/andrei/CLionProjects/Retele/BigProject/Proiect/cmake-build-debug/CMakeFiles/Proiect.dir/main.cpp.o"
+  "/home/andrei/Documents/Retele/BigProject/Proiect/main.cpp" "/home/andrei/Documents/Retele/BigProject/Proiect/cmake-build-debug/CMakeFiles/Proiect.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
