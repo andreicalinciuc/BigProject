4b4032b6c89708a03474ffc8a292f35945f1e9ec
Andrei <calinciuc.andrei@yahoo.com>
2018-11-25 01:52:23 +0200

    super

---
+++ Proiect/cmake-build-debug/CMakeFiles/Proiect.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Proiect.dir/main.cpp.o"
+  "Proiect.pdb"
+  "Proiect"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Proiect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
